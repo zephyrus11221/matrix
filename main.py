c7f8149b878c6359218823ac6dcd8c4187718fe2
Stephan Doan <sdoan@stuy.edu>
2017-02-26 22:33:34 -0500

    work on matrix, need to add edge and point

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 from display import *
 from draw import *
 
-screen = new_screen()
+'''
+tests
 color = [ 0, 255, 0 ]
 matrix = new_matrix()
+print_matrix(matrix)
 
+print '---------------------------'
+
+ident(matrix)
+print_matrix(matrix)
+
+print '---------------------------'
+
+matrix1 = new_matrix()
+ident(matrix1)
+scalar_mult(matrix1, 2)
+print_matrix(matrix1)
+
+print '---------------------------'
+
+matrix_mult(matrix, matrix1)
+print_matrix(matrix1)
+for i in range(4):
+    for p in range(4):
+        matrix[i][p]=2
+for i in range(4):
+    for p in range(4):
+        matrix1[i][p]=2
+
+print_matrix(matrix)
+matrix_mult(matrix, matrix1)
+print_matrix(matrix1)
+'''
+
+#draw
+screen = new_screen()
+color = [0, 255, 0]
+
+matrix = new_matrix(4,0)
+for i in range(101):
+    p = [i*5, 500-i*5, 0, 1]
+    p2 = [500-i*5, i*5, 0, 1]
+    matrix.append(p)
+    matrix.append(p2)
+print_matrix(matrix)
 
 draw_lines( matrix, screen, color )
 display(screen)
--- matrix.py
+++ matrix.py
@@ -4,21 +4,37 @@ import math
 def print_matrix( matrix ):
     for p in matrix:
         for i in p:
-            print i
-        print '\n'
+            print i,
+        print 
 
 def ident( matrix ):
+    for i in range(4):
+        matrix[i][i] = 1
     pass
 
 def scalar_mult( matrix, s ):
+    for p in range(len(matrix)):
+        for i in range(4):
+            matrix[p][i] *= s
     pass
 
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    hold = []
+    hold = [0,0,0,0]
+    for p in range(len(m2)):
+        hold1 = m2[p]
+        hold2 = []
+        for i in range(4):
+            for x in range(4):
+                hold2.append(m1[x][i])
+            m2[p][i]=point_mult(hold2, hold1[i])
     pass
 
-
+def point_mult(l, p):
+    otpt = 0
+    for i in range(4):
+        otpt += l[i]*p
+    return otpt
 
 
 def new_matrix(rows = 4, cols = 4):
