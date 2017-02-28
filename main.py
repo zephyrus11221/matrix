5c215504e4cd04c9c4305139be21ad3451f0038c
Stephan Doan <sdoan@stuy.edu>
2017-02-28 11:48:28 -0500

    drawing complete, tests printing

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from display import *
 from draw import *
 
-'''
-tests
+
+#tests
+print 'Testing matrix creation'
 color = [ 0, 255, 0 ]
 matrix = new_matrix()
 print_matrix(matrix)
 
-print '---------------------------'
+print '---------------------------\nTesting identity'
 
 ident(matrix)
 print_matrix(matrix)
 
-print '---------------------------'
+print '---------------------------\nTesting scalar mult'
 
 matrix1 = new_matrix()
 ident(matrix1)
 scalar_mult(matrix1, 2)
 print_matrix(matrix1)
 
-print '---------------------------'
+print '---------------------------\nTesting matrix mult'
 
 matrix_mult(matrix, matrix1)
 print_matrix(matrix1)
@@ -33,7 +34,7 @@ for i in range(4):
 print_matrix(matrix)
 matrix_mult(matrix, matrix1)
 print_matrix(matrix1)
-'''
+
 
 #draw
 screen = new_screen()
@@ -42,25 +43,46 @@ matrix1 = []
 matrix2 = []
 matrix3 = []
 matrix4 = []
+matrix5 = []
+matrix6 = []
 
 for i in range(101):
     add_edge(matrix1, i*5, 500-i*5, 0, 500-i*5, 0, 0)
 for i in range(101):
-    add_edge(matrix2, 500-i*5, i*5, 0, 0, 500-i*5, 0)
+    add_edge(matrix2, 0, i*5, 0, 500-i*5, 500-i*5, 0)
 for i in range(101):
     add_edge(matrix3, 500-i*5, 500-i*5, 0, 500, i*5, 0,)
 for i in range(101):
     add_edge(matrix4, i*5, 500-i*5, 0, 500-i*5, 500, 0)
 
-color = [0, 200, 150]    
+
+for i in range(251):
+    color = [250-i, 250-i, 250-i]
+    add_edge(matrix6, 0, 0, 0, 500, 500-i, 0)
+    add_edge(matrix6, 500, 0, 0, 0+i, 500, 0)
+    add_edge(matrix6, 500, 500, 0, 0, 0+i, 0)
+    add_edge(matrix6, 0, 500, 0, 500-i, 0, 0)
+    draw_lines( matrix6, screen, color)
+    matrix6 = []
+    
+for i in range(251):
+    color = [i, i/10, i/10]
+    add_edge(matrix5, 0, 0, 0, 500, 250-i, 0)
+    add_edge(matrix5, 500, 0, 0, 250+i, 500, 0)
+    add_edge(matrix5, 500, 500, 0, 0, 250+i, 0)
+    add_edge(matrix5, 0, 500, 0, 250-i, 0, 0)
+    draw_lines( matrix5, screen, color) 
+    matrix5 = []
+    
+color = [150, 100, 150]    
 draw_lines( matrix1, screen, color) 
-color = [100, 0, 200]    
+color = [150, 100, 100]    
+draw_lines( matrix4, screen, color) 
+
+color = [50, 0, 200]    
 draw_lines( matrix2, screen, color)
 color = [0, 0, 150]    
 draw_lines( matrix3, screen, color) 
-
-color = [50, 200, 0]    
-draw_lines( matrix4, screen, color) 
 
 
 display(screen)
